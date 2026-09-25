e044c96554edb3fdedeaef1209d4cb16753b8de5
lava <bobrich1977@gmail.com>
2026-09-26 00:53:14 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,128 +1,135 @@
-#include "raylib.h"
-#include <stdlib.h>
+#include <raylib.h>
+#include <raymath.h>
 
 #define GRID_SIZE 30
-const int screen_width = GRID_SIZE * GRID_SIZE;
-const int screen_height = GRID_SIZE * GRID_SIZE;
+#define SCREEN_WIDTH (GRID_SIZE * GRID_SIZE)
+#define SCREEN_HEIGHT (GRID_SIZE * GRID_SIZE)
+#define MAX_LENGTH (GRID_SIZE * GRID_SIZE)
+#define HEAD 0
 
-float move_timer = 0.0f;
-float move_delay = 0.1f;
-
-struct snake {
-    Vector2 position[GRID_SIZE * GRID_SIZE];
-    Vector2 speed;
+typedef struct {
+    Vector2 pos[MAX_LENGTH];
+    Vector2 dir;
     int length;
-    bool can_turn;
-};
+} snake_t;
 
-struct state {
-    struct snake snake;
+typedef struct {
+    snake_t snake;
     Vector2 food;
-    float dt;
+    float move_delay;
+    float move_timer;
+    bool can_turn;
     bool game_over;
-};
+} Context;
 
-void update(struct state *state);
-void draw(const struct state *state);
+static Context default_context = {};
 
-int main(void) {
-    InitWindow(screen_width, screen_height, "snake game");
-    SetTargetFPS(60);
-
-    struct state state = {
-        .snake = { .position = { [0] = { 20.0f, 15.0f }, [1] = { 19.0f, 15.0f }, [2] = { 18.0f, 15.0f } },
-                   .speed = { 1.0f, 0.0f },
-                   .length = 3,
-                   .can_turn = true },
-        .food = { 20.0f, 20.0f }
-    };
-    while (!WindowShouldClose()) {
-        state.dt = GetFrameTime();
-        update(&state);
-        draw(&state);
-    }
-    CloseWindow();
-    return 0;
-}
-
-void update(struct state *state) {
-    if (!state->game_over) {
-        move_timer += state->dt;
-        Vector2 *head = &state->snake.position[0];
-        // 키 입력
-        if (IsKeyPressed(KEY_W) && state->snake.speed.y != 1.0f && state->snake.can_turn) {
-            state->snake.speed = (Vector2){ 0.0f, -1.0f };
-            state->snake.can_turn = false;
-        }
-        if (IsKeyPressed(KEY_A) && state->snake.speed.x != 1.0f && state->snake.can_turn) {
-            state->snake.speed = (Vector2){ -1.0f, 0.0f };
-            state->snake.can_turn = false;
-        }
-        if (IsKeyPressed(KEY_S) && state->snake.speed.y != -1.0f && state->snake.can_turn) {
-            state->snake.speed = (Vector2){ 0.0f, 1.0f };
-            state->snake.can_turn = false;
-        }
-        if (IsKeyPressed(KEY_D) && state->snake.speed.x != -1.0f && state->snake.can_turn) {
-            state->snake.speed = (Vector2){ 1.0f, 0.0f };
-            state->snake.can_turn = false;
-        }
-        // 뱀 움직임
-        if (move_timer >= move_delay) {
-            move_timer = 0.0f;
-            for (size_t i = state->snake.length - 1; i > 0; --i) {
-                state->snake.position[i] = state->snake.position[i - 1];
+static void context_update(Context *ctx)
+{
+    if (!ctx->game_over) {
+        // Handle input
+        if (ctx->can_turn) {
+            if (IsKeyPressed(KEY_W) && ctx->snake.dir.y != 1.0f) {
+                ctx->snake.dir = (Vector2){ 0.0f, -1.0f };
+                ctx->can_turn = false;
             }
-            head->x += state->snake.speed.x;
-            head->y += state->snake.speed.y;
-            state->snake.can_turn = true;
-        }
-        // 벽 충돌
-        if (head->x < 0 || head->x >= GRID_SIZE || head->y < 0 || head->y >= GRID_SIZE) {
-            state->game_over = true;
-        }
-        // 몸 충돌
-        for (size_t i = state->snake.length - 1; i > 0; --i) {
-            if (head->x == state->snake.position[i].x && head->y == state->snake.position[i].y) {
-                state->game_over = true;
+            if (IsKeyPressed(KEY_S) && ctx->snake.dir.y != -1.0f) {
+                ctx->snake.dir = (Vector2){ 0.0f, 1.0f };
+                ctx->can_turn = false;
+            }
+            if (IsKeyPressed(KEY_A) && ctx->snake.dir.x != 1.0f) {
+                ctx->snake.dir = (Vector2){ -1.0f, 0.0f };
+                ctx->can_turn = false;
+            }
+            if (IsKeyPressed(KEY_D) && ctx->snake.dir.x != -1.0f) {
+                ctx->snake.dir = (Vector2){ 1.0f, 0.0f };
+                ctx->can_turn = false;
             }
         }
-        // 음식 충돌
-        if (head->x == state->food.x && head->y == state->food.y) {
-            state->snake.length++;
-            state->snake.position[state->snake.length - 1] = state->snake.position[state->snake.length - 2];
-            state->food = (Vector2){ (float)GetRandomValue(0, GRID_SIZE - 1), (float)GetRandomValue(0, GRID_SIZE - 1) };
+
+        // Update snake
+        ctx->move_timer += GetFrameTime();
+        if (ctx->move_timer > ctx->move_delay) {
+            ctx->move_timer = 0.0f;
+            ctx->can_turn = true;
+            for (int i = ctx->snake.length - 1; i > HEAD; --i) {
+                ctx->snake.pos[i] = ctx->snake.pos[i - 1];
+            }
+            ctx->snake.pos[HEAD] = Vector2Add(ctx->snake.pos[HEAD], ctx->snake.dir);
+        }
+
+        // Eat Food
+        if (Vector2Equals(ctx->snake.pos[HEAD], ctx->food)) {
+            ctx->food = (Vector2){ GetRandomValue(0, GRID_SIZE - 1), GetRandomValue(0, GRID_SIZE - 1) };
+            ctx->snake.pos[ctx->snake.length] = ctx->snake.pos[ctx->snake.length - 1];
+            ctx->snake.length++;
+        }
+        // Check self collision
+        for (int i = HEAD + 1; i < ctx->snake.length; ++i) {
+            if (Vector2Equals(ctx->snake.pos[HEAD], ctx->snake.pos[i])) {
+                ctx->game_over = true;
+                break;
+            }
+        }
+
+        // Check wall collision
+        if (ctx->snake.pos[HEAD].y < 0 || ctx->snake.pos[HEAD].y >= GRID_SIZE || ctx->snake.pos[HEAD].x < 0 || ctx->snake.pos[HEAD].x >= GRID_SIZE) {
+            ctx->game_over = true;
         }
     } else {
         if (IsKeyPressed(KEY_ENTER)) {
-            state->snake.length = 3;
-            state->snake.position[0] = (Vector2){ 20.0f, 15.0f };
-            state->snake.position[1] = (Vector2){ 19.0f, 15.0f };
-            state->snake.position[2] = (Vector2){ 18.0f, 15.0f };
-            state->snake.speed = (Vector2){ 1.0f, 0.0f };
-            state->snake.can_turn = true;
-
-            state->food = (Vector2){ (float)GetRandomValue(0, GRID_SIZE - 1), (float)GetRandomValue(0, GRID_SIZE - 1) };
-            move_timer = 0.0f;
-            state->game_over = false;
+            *ctx = default_context;
         }
     }
 }
 
-void draw(const struct state *state) {
+static void context_draw(Context *ctx)
+{
     BeginDrawing();
-    ClearBackground(BLACK);
+    ClearBackground(RAYWHITE);
 
-    if (!state->game_over) {
-        DrawRectangleV((Vector2){ (float)state->food.x * GRID_SIZE, state->food.y * (float)GRID_SIZE },
-                       (Vector2){ (float)GRID_SIZE, (float)GRID_SIZE }, RED);
-        for (size_t i = 0; i < state->snake.length; ++i) {
-            Vector2 pos = { state->snake.position[i].x * GRID_SIZE, state->snake.position[i].y * GRID_SIZE };
-            DrawRectangleV(pos, (Vector2){ (float)GRID_SIZE, (float)GRID_SIZE }, GREEN);
+    if (!ctx->game_over) {
+        // Draw snake
+        for (int i = 0; i < ctx->snake.length; ++i) {
+            Color color = (i == HEAD) ? DARKGREEN : GREEN;
+            DrawRectangleRec((Rectangle){ ctx->snake.pos[i].x * GRID_SIZE, ctx->snake.pos[i].y * GRID_SIZE, GRID_SIZE - 1, GRID_SIZE - 1 }, color);
         }
+
+        // Draw Food
+        DrawRectangleRec((Rectangle){ ctx->food.x * GRID_SIZE, ctx->food.y * GRID_SIZE, GRID_SIZE, GRID_SIZE }, RED);
     } else {
-        DrawText("PRESS [ENTER] TO PLAY AGAIN", screen_width / 2 - MeasureText("PRESS [ENTER] TO PLAY AGAIN", 20) / 2,
-                 screen_height / 2 - 50, 20, WHITE);
+        DrawText("GAME OVER", SCREEN_WIDTH / 2 - MeasureText("GAME OVER", 40) / 2, SCREEN_HEIGHT / 2 - 80, 40, RED);
+        DrawText("PRESS [ENTER] TO PLAY AGAIN", SCREEN_WIDTH / 2 - MeasureText("PRESS [ENTER] TO PLAY AGAIN", 20) / 2, SCREEN_HEIGHT / 2, 20, GRAY);
     }
+    DrawText(TextFormat("SCORE: %d", ctx->snake.length), SCREEN_WIDTH - MeasureText(TextFormat("SCORE: %d", ctx->snake.length), 20) - 20, 20, 20, BLACK);
 
     EndDrawing();
+}
+
+int main(void)
+{
+    Context context = { .snake = {
+                            .pos = {
+                                [0] = { 15.0f, 15.0f },
+                                [1] = { 14.0f, 15.0f },
+                                [2] = { 13.0f, 15.0f } },
+                            .dir = { 1.0f, 0.0f },
+                            .length = 3 },
+                        .food = { 18.0f, 18.0f },
+                        .move_delay = 0.075f,
+                        .move_timer = 0.0f,
+                        .can_turn = true,
+                        .game_over = false };
+
+    default_context = context;
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "snake game");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        context_update(&context);
+        context_draw(&context);
+    }
+    CloseWindow();
+    return 0;
 }
